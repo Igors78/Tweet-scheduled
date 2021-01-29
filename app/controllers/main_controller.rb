50d62fa20f5546c9950b00949682207889428e86
Igors78 <oleinikovsigors@gmail.com>
2021-01-29 12:24:42 +0200

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class MainController < ApplicationController
-    def  index
-        
-    end
-    
+  def index
+    flash[:notice] = 'Logged in successfully!'
+    flash[:alert] = 'Logged in successfully!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'main#index'
   get 'about-us', to: 'about#index', as: :about
